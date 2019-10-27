52c736ccb7a116e69f5086aae81523f745a50804
SlithyMatt <slithymatt@hotmail.com>
2019-10-27 18:31:37 -0400

    Made maketiles tool take bitmap width as an optional argument

--- maketiles.c
+++ maketiles.c
@@ -2,17 +2,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define DEFAULT_WIDTH 384
+
 void main(int argc, char **argv) {
    FILE *ifp;
    FILE *ofp;
 
-   uint8_t idata[6144];
-   uint8_t odata[6144];
+   uint8_t *idata;
+   uint8_t *odata;
+   int width;
    int address;
    int i,j,k;
 
    if (argc < 3) {
-      printf("Usage: %s [input raw data] [tile data file] [load address]\n", argv[0]);
+      printf("Usage: %s [input raw data] [tile data file] [width] [load address]\n", argv[0]);
       return;
    }
 
@@ -27,9 +30,18 @@ void main(int argc, char **argv) {
       return;
    }
 
+   if (argc >=4) {
+      width = atoi(argv[3]);
+   } else {
+      width = DEFAULT_WIDTH;
+   }
+
+   idata = malloc(width*16);
+   odata = calloc(width,16);
+
    // start 12-bit with default address
-   if (argc >= 4) {
-      sscanf(argv[3],"%x",&address);
+   if (argc >= 5) {
+      sscanf(argv[4],"%x",&address);
    } else {
       // set default load address to 0x5000
       address = 0x5000;
@@ -42,18 +54,20 @@ void main(int argc, char **argv) {
    fwrite(&odata[2],256,1,ofp);
 
    while (!feof(ifp)) {
-      if (fread(idata,1,6144,ifp) >= 6144) {
-         for (i=0;i<24;i++) {
+      if (fread(idata,1,width*16,ifp) >= 6144) {
+         for (i=0;i<width/16;i++) {
             for (j=0;j<16;j++) {
                for (k=0;k<16;k++) {
-                  odata[i*256+j*16+k] = idata[j*384+i*16+k];
+                  odata[i*256+j*16+k] = idata[j*width+i*16+k];
                }
             }
          }
-         fwrite(odata,256,24,ofp);
+         fwrite(odata,1,width*16,ofp);
       }
    }
 
    fclose(ifp);
    fclose(ofp);
+   free(idata);
+   free(odata);
 }
